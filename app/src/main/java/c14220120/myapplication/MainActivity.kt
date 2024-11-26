931fb0af36778238e2ebc7939ac7bb4a43324212
bonangansell <c14220120@john.petra.ac.id>
2024-11-26 19:00:40 +0700

    Perubahan 11

--- app/src/main/java/c14220120/myapplication/MainActivity.kt
+++ app/src/main/java/c14220120/myapplication/MainActivity.kt
@@ -1,7 +1,9 @@
 package c14220120.myapplication
 
+
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -12,17 +14,10 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var _nama : MutableList<String>
-    private lateinit var _deskripsi : MutableList<String>
-    private lateinit var _karakter : MutableList<String>
-    private lateinit var _gambar : MutableList<String>
-
-    private var arWayang = arrayListOf<wayang>()
-    private lateinit var _rvWayang : RecyclerView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -32,71 +27,114 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         _rvWayang = findViewById<RecyclerView>(R.id.rvWayang)
-        SiapkanData()
-        TambahData()
-        TampilkanData()
-    }
-    fun SiapkanData(){
-        _nama = resources.getStringArray(R.array.namaWayang).toMutableList()
-        _deskripsi = resources.getStringArray(R.array.deskripsiWayang).toMutableList()
-        _karakter = resources.getStringArray(R.array.karakterUtamaWayang).toMutableList()
-        _gambar = resources.getStringArray(R.array.gambarWayang).toMutableList()
-    }
 
-    fun TambahData(){
-        arWayang.clear()
-        for (position in _nama.indices){
-            val data = wayang(
-                _gambar[position],
-                _nama[position],
-                _karakter[position],
-                _deskripsi[position]
-            )
-            arWayang.add(data)
+        sp = getSharedPreferences("dataSP", MODE_PRIVATE)
+        val gson = Gson()
+        val isiSP = sp.getString("spWayang", null)
+        val type = object :TypeToken<ArrayList<wayang>>() {}.type
+        if (isiSP != null) {
+            arwayang = gson.fromJson(isiSP, type)
         }
-    }
-    fun TampilkanData(){
-       _rvWayang.layoutManager = LinearLayoutManager(this)
 
-        val adapterWayang = adapterRecView(arWayang)
-        _rvWayang.adapter = adapterWayang
 
-        adapterWayang.setOnItemClickCallback(object : adapterRecView.OnitemClickCallback{
-            override fun onItemClicked(data: wayang) {
-                Toast.makeText(this@MainActivity, data.nama, Toast.LENGTH_LONG).show()
-                val intent = Intent(this@MainActivity, detWayang::class.java)
-                intent.putExtra("kirimData", data)
-                startActivity(intent)
+
+        fun SiapkanData() {
+
+            _nama = resources.getStringArray(R.array.namaWayang).toMutableList()
+            _karakter = resources.getStringArray(R.array.karakterUtamaWayang).toMutableList()
+            _deskripsi = resources.getStringArray(R.array.deskripsiWayang).toMutableList()
+            _gambar = resources.getStringArray(R.array.gambarWayang).toMutableList()
+
+        }
+
+        fun tambahData() {
+            val gson = Gson()
+            val editor = sp.edit()
+            arwayang.clear()
+            for (position in _nama.indices) {
+                val wayang = wayang(
+                    _gambar[position],
+                    _nama[position],
+                    _karakter[position],
+                    _deskripsi[position]
+                )
+                arwayang.add(wayang)
             }
+            val json = gson.toJson(arwayang)
+            editor.putString("spWayang", json)
+            editor.apply()
+        }
 
-            override fun delData(pos: Int) {
-                AlertDialog.Builder(this@MainActivity)
-                    .setTitle("HAPUS DATA")
-                    .setMessage("Apakah Benar Data "+_nama[pos]+" akan dihapus ?")
-                    .setPositiveButton(
-                        "HAPUS",
-                        DialogInterface.OnClickListener { dialog, which ->
+        fun tampilkanData() {
+            _rvWayang.layoutManager = LinearLayoutManager(this)
+            _rvWayang.adapter = adapterRecView(arwayang)
+
+            val adapterwayang = adapterRecView(arwayang)
+            _rvWayang.adapter = adapterwayang
+
+
+            adapterwayang.setOnItemClickCallback(object : adapterRecView.OnitemClickCallback {
+                override fun onItemClicked(data: wayang) {
+//                   Toast.makeText(this@MainActivity,data.nama,Toast.LENGTH_LONG).show()
+                    val intent = Intent(this@MainActivity, detWayang::class.java)
+                    intent.putExtra("kirimData", data)
+                    startActivity(intent)
+
+                }
+
+                override fun delData(pos: Int) {
+                    AlertDialog.Builder(this@MainActivity)
+                        .setTitle("HAPUS DATA")
+                        .setMessage("Apakah Benar Data"+_nama[pos]+" Akan Dihapus?")
+                        .setPositiveButton("Hapus",DialogInterface.OnClickListener { dialog, which ->
                             _gambar.removeAt(pos)
                             _nama.removeAt(pos)
-                            _deskripsi.removeAt(pos)
                             _karakter.removeAt(pos)
-                            TambahData()
-                            TampilkanData()
+                            _deskripsi.removeAt(pos)
+                            arwayang.clear()
+                            tambahData()
+                            tampilkanData()
                         }
-                    )
-                    .setNegativeButton(
-                        "BATAL",
-                        DialogInterface.OnClickListener { dialog, which ->
+                        )
+                        .setNegativeButton("Batal",DialogInterface.OnClickListener { dialog, which ->
                             Toast.makeText(
                                 this@MainActivity,
                                 "Data Batal Dihapus",
                                 Toast.LENGTH_LONG
                             ).show()
                         }
-                    ).show()
-            }
+                        ).show()
 
-        })
+                }
+            })
+        }
+
+        _rvWayang = findViewById<RecyclerView>(R.id.rvWayang)
+
+        if (arwayang.size == 0) {
+            SiapkanData()
+        }else{
+            arwayang.forEach{
+                _nama.add(it.nama)
+                _karakter.add(it.karakter)
+                _deskripsi.add(it.deskripsi)
+                _gambar.add(it.foto)
+            }
+        }
+        tambahData()
+        tampilkanData()
     }
+
+    lateinit var  sp : SharedPreferences
+
+    private  var _nama: MutableList<String> = emptyList<String>().toMutableList()
+    private  var _karakter: MutableList<String> = emptyList<String>().toMutableList()
+    private  var _deskripsi: MutableList<String> =  emptyList<String>().toMutableList()
+    private  var _gambar: MutableList<String> =  emptyList<String>().toMutableList()
+
+    private lateinit var _rvWayang: RecyclerView
+
+    private var arwayang = arrayListOf<wayang>()
 }
--- app/src/main/java/c14220120/myapplication/wayang.kt
+++ app/src/main/java/c14220120/myapplication/wayang.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class wayang(
+data class  wayang(
     var foto : String,
     var nama : String,
     var karakter : String,
